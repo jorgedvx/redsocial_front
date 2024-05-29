8c3790020a47a8cc2f6f16b722e6626739672613
jorgedvx <jorgecongalopez02@gmail.com>
2024-05-28 21:45:53 -0500

    news changes 280524 api

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,7 +1,7 @@
-// export const Global = {
-//     url:"https://api-rest-red-social.onrender.com/api/"
-// }
-
 export const Global = {
-    url:"http://localhost:3900/api/"
+    url:"https://api-rest-red-social.onrender.com/api/"
 }
+
+// export const Global = {
+//     url:"http://localhost:3900/api/"
+// }
